995d47b9c4444bd5470010a525339e4ec44c7bc1
techfudge <bobobusari11@gmail.com>
2025-04-11 03:11:10 +0100

    Initial commit

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Knight',
+  author: 'Bot',
+  botName: "Knight Bot",
+  botOwner: 'Isaac®', // Your name
+  ownerNumber: '2347049704275', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
